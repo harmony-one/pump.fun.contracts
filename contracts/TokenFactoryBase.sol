27ce4e5eae0a9da5d55772600828800fab4e83d2
potvik <yuriy@harmony.one>
2025-02-12 22:13:43 +0300

    added publish to uniswap logic

--- contracts/TokenFactoryBase.sol
+++ contracts/TokenFactoryBase.sol
@@ -7,9 +7,10 @@ import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.
 import "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {BancorBondingCurve} from "./BancorBondingCurve.sol";
+import {LiquidityManager} from "./LiquidityManager.sol";
 import {Token} from "./Token.sol";
 
-contract TokenFactoryBase is Initializable, OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpgradeable {
+contract TokenFactoryBase is Initializable, OwnableUpgradeable, UUPSUpgradeable, ReentrancyGuardUpgradeable, LiquidityManager {
     uint256 public VERSION;
     uint256 public constant FEE_DENOMINATOR = 10000;
 
@@ -18,6 +19,7 @@ contract TokenFactoryBase is Initializable, OwnableUpgradeable, UUPSUpgradeable,
     uint256 public feePercent; // bp
     uint256 public feeAccumulated;
     uint256 public feeWithdrawn;
+    uint256 public requiredCollateral;
 
     mapping(uint256 => address) public winners;
     mapping(address => uint256) public collateralById;
@@ -32,18 +34,36 @@ contract TokenFactoryBase is Initializable, OwnableUpgradeable, UUPSUpgradeable,
     event TokenMinted(address indexed token, uint256 assetAmount, uint256 tokenAmount, uint256 timestamp);
     event TokenSell(address indexed token, uint256 amount0In, uint256 amount0Out, uint256 fee, uint256 timestamp);
 
+    event WinnerLiquidityAdded(
+        address indexed tokenAddress,
+        address indexed tokenCreator,
+        address indexed pool,
+        address sender,
+        uint256 tokenId,
+        uint128 liquidity,
+        uint256 actualTokenAmount,
+        uint256 actualAssetAmount,
+        uint256 timestamp
+    );
+
     function initialize(
         address _tokenImplementation,
+        address _uniswapV3Factory,
+        address _nonfungiblePositionManager,
         address _bondingCurve,
-        uint256 _feePercent
+        address _weth,
+        uint256 _feePercent,
+        uint256 _requiredCollateral
     ) public initializer {
         __Ownable_init();
         __ReentrancyGuard_init();
+        __LiquidityManager_init(_uniswapV3Factory, _nonfungiblePositionManager, _weth);
 
         tokenImplementation = _tokenImplementation;
         VERSION = 20241221;
         bondingCurve = BancorBondingCurve(_bondingCurve);
         feePercent = _feePercent;
+        requiredCollateral = _requiredCollateral;
     }
 
     /// @dev Required by UUPSUpgradeable to authorize upgrades
@@ -53,6 +73,10 @@ contract TokenFactoryBase is Initializable, OwnableUpgradeable, UUPSUpgradeable,
 
     function setBondingCurve(address _bondingCurve) external onlyOwner {
         bondingCurve = BancorBondingCurve(_bondingCurve);
+    }
+
+    function setRequiredCollateral(uint256 _requiredCollateral) external onlyOwner {
+        requiredCollateral = _requiredCollateral;
     }
 
     function setFeePercent(uint256 _feePercent) external onlyOwner {
@@ -79,6 +103,7 @@ contract TokenFactoryBase is Initializable, OwnableUpgradeable, UUPSUpgradeable,
 
     function _buy(address tokenAddress, address receiver, uint256 paymentAmount) internal returns (uint256) {
         require(paymentAmount > 0, "ETH not enough");
+        require(tokensPools[tokenAddress] == address(0), "Token is traded only on Uniswap");
 
         Token token = Token(tokenAddress);
         (uint256 paymentWithoutFee, uint256 fee) = _getCollateralAmountAndFee(paymentAmount);
@@ -120,6 +145,8 @@ contract TokenFactoryBase is Initializable, OwnableUpgradeable, UUPSUpgradeable,
 
     function _sell(address tokenAddress, uint256 tokenAmount, address from, address to) internal returns (uint256) {
         require(tokenAmount > 0, "Amount should be greater than zero");
+        require(tokensPools[tokenAddress] == address(0), "Token is traded only on Uniswap");
+
         Token token = Token(tokenAddress);
         uint256 paymentAmountWithFee = bondingCurve.computeRefundForBurning(collateralById[tokenAddress], token.totalSupply(), tokenAmount);
         collateralById[tokenAddress] -= paymentAmountWithFee;
@@ -146,5 +173,25 @@ contract TokenFactoryBase is Initializable, OwnableUpgradeable, UUPSUpgradeable,
         (bool success,) = owner().call{value: feeWithdrawable}("");
         require(success, "transfer failed");
         feeWithdrawn += feeWithdrawable;
+    }
+
+    function publishToUniswap(address tokenAddress) external nonReentrant {
+        uint256 currentCollateral = collateralById[tokenAddress];
+        uint256 mintAmount;
+        {
+            require(tokensPools[tokenAddress] == address(0), "Token pool already created");
+            require(currentCollateral >= requiredCollateral, "Current collateral is too low");
+        }
+        {
+            uint256 numTokensPerEther = bondingCurve.computeMintingAmountFromPrice(currentCollateral, Token(tokenAddress).totalSupply(), 1 ether);
+            WETH.deposit{value: currentCollateral}();
+            mintAmount = (currentCollateral * numTokensPerEther) / 1e18;
+            Token(tokenAddress).mint(address(this), mintAmount);
+        }
+        (address pool, uint256 tokenId, uint128 liquidity, uint256 actualTokenAmount, uint256 actualAssetAmount) = _mintLiquidity(tokenAddress, mintAmount, currentCollateral, address(this));
+
+        tokensPools[tokenAddress] = pool;
+        liquidityPositionTokenIds[tokenAddress] = tokenId;
+        emit WinnerLiquidityAdded(tokenAddress, tokensCreators[tokenAddress], pool, msg.sender, tokenId, liquidity, actualTokenAmount, actualAssetAmount, block.timestamp);
     }
 }
--- scripts/deployAndTest.js
+++ scripts/deployAndTest.js
@@ -1,7 +1,10 @@
 const { ethers } = require("hardhat");
+const NonfungiblePositionManagerArtifact = require("@uniswap/v3-periphery/artifacts/contracts/NonfungiblePositionManager.sol/NonfungiblePositionManager.json");
 
 async function main() {
     const [owner, user] = await ethers.getSigners();
+
+    const weth = { address: "0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a" };
 
     // Deploy Token contract
     const Token = await hre.ethers.getContractFactory("Token");
@@ -15,11 +18,38 @@ async function main() {
     await bondingCurve.deployed();
     console.log(`BancorBondingCurve deployed at: ${bondingCurve.address}`);
 
+    // UniswapV3 Factory address
+    let uniswapV3FactoryAddress = "0x12d21f5d0ab768c312e19653bf3f89917866b8e8";
+
+    // Deploy PositionManager contract
+    const PositionManager = new ethers.ContractFactory(
+        NonfungiblePositionManagerArtifact.abi,
+        NonfungiblePositionManagerArtifact.bytecode,
+        owner
+    );
+
+    const positionManager = await PositionManager.deploy(uniswapV3FactoryAddress, weth.address, ethers.constants.AddressZero);
+    await positionManager.deployed();
+    console.log("NonfungiblePositionManager deployed to:", positionManager.address);
+
+    // Deploy TokenFactoryUpgradeable proxy contract
     console.log("Deploying TokenFactory...");
-    const TokenFactory = await ethers.getContractFactory("TokenFactoryBase");
-    const tokenFactory = await TokenFactory.deploy();
-    await tokenFactory.initialize(tokenImplementation.address, bondingCurve.address, 100);
-    console.log(`TokenFactory deployed at: ${tokenFactory.address}`);
+    const TokenFactoryUpgradeable = await ethers.getContractFactory("TokenFactoryBase");
+    const tokenFactory = await upgrades.deployProxy(
+        TokenFactoryUpgradeable,
+        [
+            tokenImplementation.address,
+            uniswapV3FactoryAddress,
+            positionManager.address,
+            bondingCurve.address,
+            weth.address,
+            100, // _feePercent
+            ethers.utils.parseEther("1")
+        ],
+        { initializer: "initialize" }
+    );
+    await tokenFactory.deployed();
+    console.log("TokenFactoryUpgradeable deployed to:", tokenFactory.address);
 
     console.log("Creating Token...");
     const tx = await tokenFactory.createToken("TestToken", "TTK", "testuri");
@@ -41,6 +71,15 @@ async function main() {
     console.log("Withdrawing Fees...");
     await tokenFactory.withdrawFee();
     console.log("Fees withdrawn.");
+
+    console.log("Buying Token...");
+    await tokenFactory.connect(owner).buy(tokenAddress, { value: ethers.utils.parseEther("2") });
+
+    console.log("publishToUniswap...");
+    await tokenFactory.connect(owner).publishToUniswap(tokenAddress);
+
+    console.log("Try Buying Token after publsih...");
+    await tokenFactory.connect(owner).buy(tokenAddress, { value: ethers.utils.parseEther("1") });
 }
 
 main()
